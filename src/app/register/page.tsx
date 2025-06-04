516a631e90b5f033a67d0952576795e38d88670f
agungg0 <119163133+agungg0@users.noreply.github.com>
2025-06-04 13:17:27 +0700

    add regis ui

---
+++ src/app/register/page.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography
+} from '@mui/material';
+
+export default function UserRegisterPage() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleRegister = async () => {
+    if (!username || !email || !password) {
+      alert('Semua field wajib diisi.');
+      return;
+    }
+
+    const res = await fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, email, password }),
+    });
+
+    if (res.ok) {
+      alert('Registrasi berhasil!');
+      router.push('/login');
+    } else {
+      const data = await res.json();
+      alert(`Registrasi gagal: ${data.message}`);
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8, p: 4, boxShadow: 3, borderRadius: 2 }}>
+        <Typography variant="h4" color="text.primary" gutterBottom>
+          User Registration
+        </Typography>
+        <TextField
+          fullWidth
+          label="Email"
+          margin="normal"
+          onChange={(e) => setEmail(e.target.value)}
+          InputLabelProps={{ shrink: true }}
+          sx={{
+            '& label': {
+                color: 'primary.main',
+            },
+            '& label.Mui-focused': {
+                color: 'primary.main',
+            }
+            }}
+        />
+        <TextField
+          fullWidth
+          label="Username"
+          type="email"
+          margin="normal"
+          onChange={(e) => setUsername(e.target.value)}
+          InputLabelProps={{ shrink: true }}
+          sx={{
+            '& label': {
+                color: 'primary.main',
+            },
+            '& label.Mui-focused': {
+                color: 'primary.main',
+            }
+            }}
+        />
+        <TextField
+          fullWidth
+          label="Password"
+          type="password"
+          margin="normal"
+          onChange={(e) => setPassword(e.target.value)}
+          InputLabelProps={{ shrink: true }}
+          sx={{
+            '& label': {
+                color: 'primary.main',
+            },
+            '& label.Mui-focused': {
+                color: 'primary.main',
+            }
+            }}
+        />
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+          onClick={handleRegister}
+        >
+          Register
+        </Button>
+      </Box>
+    </Container>
+  );
+}
