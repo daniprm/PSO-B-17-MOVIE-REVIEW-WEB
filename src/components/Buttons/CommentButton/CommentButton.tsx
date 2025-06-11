df09b95960bcf8dbc8cf1d342e7e7eac11a777cd
daniprm <daniprmnn@gmail.com>
2025-06-12 01:31:02 +0700

    add comment feature

--- src/components/Buttons/CommentButton/CommentButton.tsx
+++ src/components/Buttons/CommentButton/CommentButton.tsx
@@ -1,4 +1,6 @@
-import { Button } from "@mui/material";
+import { Button } from '@mui/material';
+import { createClient } from '@/Utilities/supabase/server';
+import { supabase } from '@/db/supabaseClient';
 
 interface PropsType {
   movieId: string;
@@ -7,24 +9,24 @@ interface PropsType {
   setComment: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const CommentButton = ({
+const CommentButton = async ({
   movieId,
   comment,
   onCommentAdded,
   setComment,
 }: PropsType) => {
+  const supabaseServer = createClient();
+  const user = (await supabaseServer).auth.getUser();
+  const userId = (await user).data.user?.id;
   const handleComment = async () => {
     if (comment) {
-      await fetch("http://localhost:5000/comments", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ movieId: movieId, comment: comment }),
-      });
+      const { error } = await supabase
+        .from('comments')
+        .insert({ movie_id: movieId, user_id: userId, comment: comment });
+      if (error) throw error;
 
       onCommentAdded();
-      setComment("");
+      setComment('');
     }
   };
 
